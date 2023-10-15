bff1595494c9512ff8ba3d33f1da87326d34dca7
John Pandis <ioanpand@outlook.com>
2023-10-16 00:45:11 +0300

    Update Utilities.cs
    Added more functions and summaries

--- Assets/Scripts/Utilities.cs
+++ Assets/Scripts/Utilities.cs
@@ -2,16 +2,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Utilities : MonoBehaviour {
-    /*private static Vector3 RandomVectorPointInCircle(Vector3 center, float radius, Vector3 forwardDirection) {
-        Vector2 vector2 = Random.insideUnitCircle * radius;
+    /// <summary>
+    /// Generate a random Vector3 inside the given circle
+    /// </summary>
+    /// <param name="center">Center of the circle</param>
+    /// <param name="radius">Radius of the circle</param>
+    /// <param name="forwardDirection">Orientation of the circle</param>
+    /// <returns>The generated Vector3</returns>
+    public static Vector3 RandomVectorPointInCircle(Vector3 center, float radius, Vector3 forwardDirection) {
+        var vector2 = Random.insideUnitCircle * radius;
         float signedAngle = Vector3.SignedAngle(forwardDirection, vector2, Vector3.up);
         if (signedAngle > 90 || signedAngle < -90) {
             vector2 = -vector2;
         }
         return new Vector3(vector2.x, 0, vector2.y) + center;
     }
-
-    private static bool OutOfBounds(Vector3 point, float limitsX, float limitsZ) {
+    
+    /// <summary>
+    /// Checks of the given Vector3 is inside the given box
+    /// </summary>
+    /// <param name="transform">Transfom of the object calling the function</param>
+    /// <param name="point">Position to check</param>
+    /// <param name="limitsX">Box size X</param>
+    /// <param name="limitsZ">Box size Z</param>
+    /// <returns></returns>
+    public static bool OutOfBounds(Transform transform, Vector3 point, float limitsX, float limitsZ) {
         Vector3 pointWorldToLocal = transform.InverseTransformPoint(point);
         bool outOfBoundsUp    = pointWorldToLocal.z - limitsZ / 2 > 0;
         bool outOfBoundsDown  = pointWorldToLocal.z + limitsZ / 2 < 0;
@@ -20,14 +35,50 @@ public class Utilities : MonoBehaviour {
         return outOfBoundsUp || outOfBoundsDown || outOfBoundsRight || outOfBoundsLeft;
     }
 
-    private static bool IsPositionCloseToAnyPosition(Vector3 position, List<Vector3> positions, float distance) {
+    /// <summary>
+    /// Checks if the given position is close to any of the positions in a list
+    /// </summary>
+    /// <param name="position">Position to check</param>
+    /// <param name="positions">List of positions</param>
+    /// <param name="distance">Distance</param>
+    /// <returns>True, if the distance is less than the specified 'distance'</returns>
+    public static bool IsPositionCloseToAnyPosition(Vector3 position, List<Vector3> positions, float distance) {
         bool tooClose = false;
         for (int i = 0; i < positions.Count; i++) {
-            if (Vector3.Distance(positions[i], position) <= distance) {
+            if (Vector3.Distance(position, positions[i]) <= distance) {
                 tooClose = true;
                 break;
             }
         }
         return tooClose;
-    }*/
+    }
+
+    /// <summary>
+    /// Checks if the given position is close to any of the positions in a list
+    /// </summary>
+    /// <param name="position">Position to check</param>
+    /// <param name="positions">List of positions</param>
+    /// <param name="distances">List of distances</param>
+    /// <returns>True, if the distance is less than the specified 'distances'</returns>
+    public static bool IsPositionCloseToAnyPosition(Vector3 position, List<Vector3> positions, List<float> distances) {
+        bool tooClose = false;
+        for (int i = 0; i < positions.Count; i++) {
+            if (Vector3.Distance(position, positions[i]) <= distances[i]) {
+                tooClose = true;
+                break;
+            }
+        }
+        return tooClose;
+    }
+
+    public static bool ShootRayToPosition(Transform transform, Vector3 position, float distance, int layer) {
+        position.y = transform.position.y;
+        Ray ray = new(position + Vector3.up * distance / 2, Vector3.down);
+        return Physics.Raycast(ray, distance, layer);
+    }
+    public static bool ShootRayToPosition(Transform transform, out RaycastHit hit, Vector3 position, float distance, int layer) {
+        position.y = transform.position.y;
+        Ray ray = new(position + Vector3.up * distance / 2, Vector3.down);
+        return Physics.Raycast(ray, out hit, distance, layer);
+    }
 }
